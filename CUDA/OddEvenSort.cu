e8f739911b3ac803fd68499086dd3f8cf0adb305
caleb-bynum <Caleb.bynum@tamu.edu>
2023-11-08 13:31:22 -0600

    updated CUDA pseudocode

--- CUDA/OddEvenSort.cu
+++ CUDA/OddEvenSort.cu
@@ -1,1 +1,34 @@
-/* To Do */
+
+/* CUDA Implementation Pseudocode
+
+Variables:
+    n : number of elements to sorted
+    t : number of threads
+    A : array to be sorted
+
+Idea: avoid coordination between threads by strategically launching kernels
+      and subsequently synchronzing.
+
+Main Procedure:
+    1. Copy A to GPU memory
+    2. for each phase in {0, 1, ..., n-1}:
+    3.   if phase is even:
+    4.     launch EvenPhase kernel
+    5.   else:
+    6.     launch OddPhase kernel
+    7.   synchronize device
+    8. Copy A to device memory
+
+EvenPhase Kernel:
+    1. Id = threadId + blockDim * blockId
+    2. index1 = 2 * Id
+    3. index2 = index1 + 1
+    4. compare and swap array elements at index1, index2
+
+OddPhase Kernel:
+    1. Id = threadId + blockDim * blockId
+    2. index1 = 2 * Id + 1
+    3. index2 = index1 + 1
+    4. if thread is not the last thread
+    5.   compare and swap array elements at index1, index2
+*/
