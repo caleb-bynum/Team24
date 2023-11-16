0e69be0ddccf1c8e9787b5e0c014ec36ef80386f
Caleb Josiah Bynum <caleb.bynum@login3.cluster>
2023-11-15 23:31:22 -0600

    restructure for merge

---
+++ OddEvenMPI/Merge.h
@@ -0,0 +1,51 @@
+/*
+Contains two sub-procedures:
+- Merge_High()
+- Merge_Low()
+*/
+
+/* Given sorted arrays A and B, populate C with the larger half of elements from A and B.
+   Then, copy the elements of C back into A. */
+void Merge_High(double* local_A, double* temp_B, double* temp_C, int local_n) {
+    int ai = local_n - 1;
+    int bi = local_n - 1;
+    int ci = local_n - 1;
+
+    /* perform merge of A and B --> place into C */
+    while (ci >= 0) {
+        if (local_A[ai] >= temp_B[bi]) {
+            temp_C[ci--] = local_A[ai--];
+        }
+        else {
+            temp_C[ci--] = temp_B[bi--];
+        }
+    }
+
+    /* copy elements from C to A */
+    for (int i = 0; i < local_n; i++) {
+        local_A[i] = temp_C[i];
+    }
+}
+
+/* Given sorted arrays A and B, populate C with the smaller half of elements from A and B.
+   Then, copy the elements of C back into A. */
+void Merge_Low(double* local_A, double* temp_B, double* temp_C, int local_n) {
+    int ai = 0;
+    int bi = 0;
+    int ci = 0;
+
+    /* perform merge */
+    while (ci < local_n) {
+        if (local_A[ai] <= temp_B[bi]) {
+            temp_C[ci++] = local_A[ai++];
+        }
+        else {
+            temp_C[ci++] = temp_B[bi++];
+        }
+    }
+
+    /* copy elements from C to A */
+    for (int i = 0; i < local_n; i++) {
+        local_A[i] = temp_C[i];
+    }
+}
