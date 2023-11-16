abcf0e0f70e0022ed79c2b835f269422e9d06a99
josefmun9902 <josefmun9902@gmail.com>
2023-11-15 22:06:45 -0600

    cali code

--- CUDA/sort.cu
+++ CUDA/sort.cu
@@ -1,13 +1,23 @@
-#include <cuda_runtime.h>
+// #include <cuda_runtime.h>
 #include <thrust/device_vector.h>
 #include <thrust/sort.h>
 #include <thrust/copy.h>
 #include <thrust/execution_policy.h>
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
 
-#define BUCKETS 1024
-#define THREADS_PER_BLOCK 256
+int BUCKETS;
+int THREADS_PER_BLOCK;
+const char *data_init = "data_init";
+const char *comm = "comm";
+const char *cudaMemcpy_region = "cudaMemcpy";
+const char *correctness_check = "correctness_check";
 
 __global__ void bucket_sort_kernel(float *data, int *bucket_counters, int elements, int buckets)
 {
@@ -29,27 +39,43 @@ void bucket_sort(float *data, int elements)
     cudaMalloc((void **)&d_bucket_counters, BUCKETS * sizeof(int));
     cudaMemset(d_bucket_counters, 0, BUCKETS * sizeof(int));
 
+    // CALI_MARK_BEGIN(comm);
     cudaMemcpy(d_data, data, elements * sizeof(float), cudaMemcpyHostToDevice);
+    // CALI_MARK_END(comm);
+
     bucket_sort_kernel<<<(elements + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_data, d_bucket_counters, elements, BUCKETS);
     cudaDeviceSynchronize();
 
     std::vector<int> h_bucket_counters(BUCKETS);
+
+    // CALI_MARK_BEGIN(cudaMemcpy_region);
     cudaMemcpy(h_bucket_counters.data(), d_bucket_counters, BUCKETS * sizeof(int), cudaMemcpyDeviceToHost);
+    // CALI_MARK_END(cudaMemcpy_region);
 
     thrust::sort(thrust::device, d_data, d_data + elements);
+    // CALI_MARK_BEGIN(cudaMemcpy_region);
     cudaMemcpy(data, d_data, elements * sizeof(float), cudaMemcpyDeviceToHost);
+    // CALI_MARK_END(cudaMemcpy_region);
 
     cudaFree(d_data);
     cudaFree(d_bucket_counters);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    const int num_elements = 100000;
+    cali::ConfigManager mgr;
+    mgr.start();
+
+    BUCKETS = atoi(argv[1]);
+    const int num_elements = atoi(argv[2]);
+    THREADS_PER_BLOCK = BUCKETS / 4;
+
     std::vector<float> data(num_elements);
 
+    CALI_MARK_BEGIN(data_init);
     for (int i = 0; i < num_elements; ++i)
         data[i] = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    CALI_MARK_END(data_init);
 
     bucket_sort(data.data(), num_elements);
 
@@ -57,5 +83,22 @@ int main()
         std::cout << data[i] << ' ';
     std::cout << std::endl;
 
-    return 0;
+    adiak::init(NULL);
+    adiak::launchdate();                           // launch date of the job
+    adiak::libraries();                            // Libraries used
+    adiak::cmdline();                              // Command line used to launch the job
+    adiak::clustername();                          // Name of the cluster
+    adiak::value("Algorithm", "SampleSort");       // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
+    adiak::value("ProgrammingModel", "CUDA");      // e.g., "MPI", "CUDA", "MPIwithCUDA"
+    adiak::value("Datatype", "float");             // The datatype of input elements (e.g., double, int, float)
+    adiak::value("SizeOfDatatype", sizeof(float)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+    adiak::value("InputSize", num_elements);       // The number of elements in input dataset (1000)
+    adiak::value("InputType", "Random");
+    adiak::value("num_threads", BUCKETS);                                           // The number of CUDA or OpenMP threads
+    adiak::value("num_blocks", THREADS_PER_BLOCK);                                  // The number of CUDA blocks
+    adiak::value("group_num", 24);                                                  // The number of your group (integer, e.g., 1, 10)
+    adiak::value("implementation_source", "Handwritten with some reference to AI"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
+
+    mgr.stop();
+    mgr.flush();
 }
--- MPI/sort.cpp
+++ MPI/sort.cpp
@@ -1,6 +1,17 @@
 #include "mpi.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
+
+const char* data_init = "data_init";    
+const char* scatter = "scatter";
+const char* comp_small = "comp_small";
+const char* comp_large = "comp_large";
+const char* scatterv = "scatterv";
+const char* gatherv = "gatherv";
 
 int compare(const void* first, const void* second){
     double diff = *(double*)first - *(double*)second;
@@ -28,6 +39,10 @@ void custom_qsort(double *input, int input_size){
 
 
 int main(int argc, char *argv[]){
+    CALI_CXX_MARK_FUNCTION;
+    cali::ConfigManager mgr;
+    mgr.start();
+
     int rank, P;
     int bucket_size[1];
     int N = strtol(argv[1], NULL, 10); // N numbers to be sorted
@@ -43,34 +58,46 @@ int main(int argc, char *argv[]){
 
     double start_time = MPI_Wtime();
 
+    CALI_MARK_BEGIN(data_init);
     if(rank == 0){
         for(int i = 0; i < N; i++){
             pivots[i] = (double)rand() / (double) RAND_MAX;
             bucket_num[find_bucket(pivots[i], P)]++;
         }
     }
+    CALI_MARK_END(data_init);
 
+    CALI_MARK_BEGIN(scatter);
     MPI_Scatter(bucket_num, 1, MPI_INT, bucket_size, 1, MPI_INT, 0, MPI_COMM_WORLD );
+    CALI_MARK_END(scatter);
     double *bucket_list = (double*)malloc(bucket_size[0]*sizeof(double));
 
     if(rank == 0){
         displacements[0] = 0;
+        CALI_MARK_BEGIN(comp_small);
         for(int j = 1; j < P; j++)
             displacements[j] = bucket_num[j-1] + displacements[j-1];
+        CALI_MARK_END(comp_small);
 
         int bucket;
+        CALI_MARK_BEGIN(comp_large);
         for(int j = 0; j < N; j++){
             bucket = find_bucket(pivots[j], P);
             d_list[displacements[bucket] + index[bucket]] = pivots[j];
             index[bucket]++;
         }
+        CALI_MARK_END(comp_large);
         free(pivots);
         free(index);
     }
 
+    CALI_MARK_BEGIN(scatterv);
     MPI_Scatterv(d_list, bucket_num, displacements, MPI_DOUBLE, bucket_list, bucket_size[0], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    CALI_MARK_END(scatterv);
     custom_qsort(bucket_list, bucket_size[0]);
+    CALI_MARK_BEGIN(gatherv);
     MPI_Gatherv(bucket_list, bucket_size[0], MPI_DOUBLE, d_list, bucket_num, displacements, MPI_DOUBLE, 0, MPI_COMM_WORLD );
+    CALI_MARK_END(gatherv);
     double end_time = MPI_Wtime();
 
     free(bucket_list);
@@ -80,6 +107,21 @@ int main(int argc, char *argv[]){
         free(bucket_num);
         free(d_list);
         printf("\nTotal Execution Time: %f\n", end_time - start_time);
+
+        adiak::init(NULL);
+        adiak::launchdate();
+        adiak::libraries();
+        adiak::cmdline();
+        adiak::clustername();
+        adiak::value("Algorithm", "SampleSort");
+        adiak::value("ProgrammingModel", "MPI");
+        adiak::value("Datatype", "float");
+        adiak::value("SizeOfDatatype", sizeof(float));
+        adiak::value("InputSize", N); // The number of elements in input dataset (1000)
+        adiak::value("InputType", "float"); 
+        adiak::value("num_threads", P); // The number of CUDA or OpenMP threads
+        adiak::value("group_num", 24); // The number of your group (integer, e.g., 1, 10)
+        adiak::value("implementation_source", "Mix of Handwritted and AI and Online"); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
     }
     MPI_Finalize();
 }
